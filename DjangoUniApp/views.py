a4070d1898d18e5caab8a6036b51f78d6f631c77
Giovanny <giovanny.paz97c@gmail.com>
2023-03-24 11:19:27 -0500

    plantilla perfil y productos pendiente conecciones

--- DjangoUniApp/views.py
+++ DjangoUniApp/views.py
@@ -4,6 +4,6 @@ from django.template import Template, Context
 
 def index (request):
    
-    return render(request, 'DjangoUniApp/resultados.html')
+    return render(request, 'DjangoUniApp/perfil2.html')
 
     
